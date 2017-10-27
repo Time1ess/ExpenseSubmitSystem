3acaebae375c48d0f36bd38f95fcfc4dda887277
SummerWind1995 <1561726052@qq.com>
2017-10-27 18:28:28 +0800

    docker

---
+++ web/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Initializing Django server"
+cd ExpenseSubmitSystem
+echo "Pulling new changes from git repository"
+git pull
+echo "Installing requirements"
+pip install -r requirements.txt
+echo "Collecting static files"
+python manage.py collectstatic -c --noinput
+echo "Migrating databases"
+python manage.py migrate
+echo "Compiling messages"
+python manage.py compilemessages
+echo "Starting uwsgi"
+uwsgi --ini web/ExpenseSubmitSystem_uwsgi.ini
